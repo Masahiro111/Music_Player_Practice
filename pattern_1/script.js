156f58456be1b0af65f01970c045a056bffdcc35
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-02-04 19:42:51 +0900

    save

--- pattern_1/script.js
+++ pattern_1/script.js
@@ -1,1 +1,26 @@
-const = musicContainer = document.querySelector('.music-container');
+const  musicContainer = document.querySelector('.music-container');
+const  playBtn = document.querySelector('#play');
+const  prevBtn = document.querySelector('#prev');
+const  nextBtn = document.querySelector('#next');
+const  audio = document.querySelector('#audio');
+const  progress = document.querySelector('.progress');
+const  progressContainer = document.querySelector('.progress-container');
+const  title = document.querySelector('#title');
+const  cover = document.querySelector('#cover');
+
+// song titles
+
+const songs = ['hey', 'summer', 'ukulele'];
+
+// keep track of songs
+let songIndex = 2;
+
+// Initially load song info DOM
+loadSong(songs[songIndex]);
+
+// update song details
+function loadSong(song){
+    title.innerText = song;
+    audio.src = 'music/${song}.mp3';
+    cover.src = 'music/${song}.jpg';
+}
