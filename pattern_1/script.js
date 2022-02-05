74de7e465375f6fd26274f9c08223de03e7e3463
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-02-05 18:39:04 +0900

    fix link

--- pattern_1/script.js
+++ pattern_1/script.js
@@ -21,21 +21,58 @@ loadSong(songs[songIndex]);
 // update song details
 function loadSong(song){
     title.innerText = song;
-    audio.src = 'music/${song}.mp3';
-    cover.src = 'music/${song}.jpg';
+    audio.src = `./music/${song}.mp3`;
+    cover.src = `./images/${song}.jpg`;
 }
 
 
 function playSong() {
-musicContainer.classList.add('play');
-playBtn.querySelector('i.fas').classList.remove('fa-play');
-playBtn.querySelector('i.fas').classList.add('fa-pause');
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
 }
 
 function pauseSong() {
     musicContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.add('fa-play');
     playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+
+function prevSong() {
+    songIndex--;
+
+    if(songIndex < 0){
+        songIndex = songs.length -1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+
+    if(songIndex > songs.length -1){
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function updateProgress(e) {
+const {duration, currentTime} = e.srcElement;
+const progressPercent = (currentTime / duration) * 100;
+
+progress.style.width = `${progressPercent}%`
+    console.log(e.srcElement.currentTime);
 }
 
 // Event listeners
@@ -49,3 +86,8 @@ playBtn.addEventListener('click', () => {
     }
 
 });
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate',updateProgress);
