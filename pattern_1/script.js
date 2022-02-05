adf5f3dae1ff6a8d3c02b20a9e27d6f1db2a55b1
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-02-05 19:01:24 +0900

    finished misuc player

--- pattern_1/script.js
+++ pattern_1/script.js
@@ -72,7 +72,14 @@ const {duration, currentTime} = e.srcElement;
 const progressPercent = (currentTime / duration) * 100;
 
 progress.style.width = `${progressPercent}%`
-    console.log(e.srcElement.currentTime);
+}
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = ( clickX / width ) * duration;
 }
 
 // Event listeners
@@ -91,3 +98,7 @@ prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
 audio.addEventListener('timeupdate',updateProgress);
+
+progressContainer.addEventListener('click', setProgress);
+
+audio.addEventListener('ended', nextSong);
